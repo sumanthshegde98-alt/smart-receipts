fee93c9568f479925f1d1f8449b700fb6367287a
sumanth-hegde <sumanthsh1998@gmail.com>
2025-07-27 08:31:21 +0530

    changes with ai agent

--- receipt_reader/receipt_reader/settings.py
+++ receipt_reader/receipt_reader/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -137,3 +135,6 @@ BASE_DIR
 )
 
 MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
